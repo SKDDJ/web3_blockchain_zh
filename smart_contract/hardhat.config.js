2f417ddacb06ba4a0b207eaf466972cb7164dd39
Yiming Shi <yimingshi666@gmail.com>
2024-11-09 18:56:51 +0800

    feat: support zh

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,15 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 module.exports = {
-  solidity: '0.8.0',
+  solidity: "0.8.24",
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    hardhat: {
+      chainId: 1337
     },
   },
 };
